3d35ce52515ab42d4c4154f060ced05e7ef7b4cf
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-05-01 22:56:28 +0500

    edit example

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import AppLayout from "./layouts/AppLayout";
 import MainLayout from "./layouts/MainLayout";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
+import Home from "./Components/Home/Home";
 import AdminLayout from "./layouts/AdminLayout";
 import ProtectedRoute from "./Components/ProtectedRoute"; // Импорт компонента защиты маршрутов
-import UI from "./Pages/UI";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/example/UI" element={<UI />} />
         <Route path="/" element={<AppLayout />}>
           <Route
             element={
